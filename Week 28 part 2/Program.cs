89f7060e0f0875de7744190036d0a30b02cae7ec
Aaron Willcox (Student) <WIL16001766@btc.ac.uk>
2021-05-13 16:20:54 +0100

    Add project files.

---
+++ Week 28 part 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week_28_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("YE0 T0DD", "Ford", "Fiesta");
+            TPSolutions.AddCar(c1);
+
+            Ticket t1 = new Ticket(c1, 75.0, 14);
+            TPSolutions.AddTicket(t1);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(t1);
+        }
+    }
+}
---
+++ Week 28 part 2/Ticket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_28_part_2
+{
+    class Ticket
+    {
+        public Guid Id { get; set; }
+
+        public Car Car { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double Fee { get; set; }
+
+        public int DaysToPay { get; set; }
+
+        public bool Paid { get; set; }
+
+        public Ticket(Car car, double fee, int daysToPay)
+        {
+            Id = Guid.NewGuid();
+            Car = car;
+            Date = DateTime.Now;
+            Fee = fee;
+            DaysToPay = daysToPay;
+            Paid = false;
+        }
+
+        public override string ToString()
+        {
+            return $@"
+Id: {Id} 
+Car: {Car} 
+Date: {Date} 
+Fee: {Fee} 
+DaysToPay: {DaysToPay} 
+Paid: {Paid}";
+        }
+    }
+}
